dee67c2836c3ad71404d3047703c2b79a221afd6
luyang6137 <luyang6137@qq.com>
2023-09-14 18:10:29 +0800

    Update lytest.thrift

--- thrift_idl/lytest.thrift
+++ thrift_idl/lytest.thrift
@@ -6,7 +6,7 @@ include "order/user_order.thrift"
 
 service ApiService {
     // 创建管理员-lytest
-    admin_user.CreateAdminUserResponse1 CreateAdminUser1(1: admin_user.CreateAdminUserRequest1 req) (api.post="/eduosapi/admin_user/lytest", api.name="创建管理员_lytest")
+    admin_user.CreateAdminUserResponse1 CreateAdminUser1(1: admin_user.CreateAdminUserRequest1 req) (api.post="/eduosapi/admin_user/lytest/lytest1/lytest2/lytest3/lytest4/lytest5/lytest6/lytest7", api.name="创建管理员_lytest")
     // 相同请求方式及请求路径测试
     admin_user.CreateAdminUserResponse2 CreateAdminUser2(1: admin_user.CreateAdminUserRequest2 req) (api.post="/samepath", api.name="相同路径_lytest")
     // 相同请求方式及请求路径测试2
