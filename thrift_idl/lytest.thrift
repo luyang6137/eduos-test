aa0260cfa87bd9e8c7f29ecf4156bb7411f2ae27
luyang6137 <luyang6137@qq.com>
2023-09-15 10:47:05 +0800

    Update lytest.thrift

--- thrift_idl/lytest.thrift
+++ thrift_idl/lytest.thrift
@@ -11,4 +11,6 @@ service ApiService {
     admin_user.CreateAdminUserResponse2 CreateAdminUser2(1: admin_user.CreateAdminUserRequest2 req)  (api.post="/samepath", api.name="相同路径_lytest")
     // 相同请求方式及请求路径测试2
     admin_user.CreateAdminUserResponse3 CreateAdminUser3(1: admin_user.CreateAdminUserRequest3 req)  (api.post="/samepath", api.name="相同路径_lytest")
+     // 业务测试
+    admin_user.CreateAdminUserResponse4 CreateAdminUser4(1: admin_user.CreateAdminUserRequest3 req)  (api.post="/status/200", api.name="返回200")
 }
