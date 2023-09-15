8c3126d01a0fc473006b38a7d2fde65427ed61ec
luyang6137 <luyang6137@qq.com>
2023-09-15 10:57:12 +0800

    Update lytest.thrift

--- thrift_idl/lytest.thrift
+++ thrift_idl/lytest.thrift
@@ -1,16 +1,14 @@
 
 include "common.thrift"
-include "user/admin_user.thrift"
-include "order/user_order.thrift"
-#include "bucunzai.thrift"
+include "lytest/lytest.thrift"
 
 service ApiService {
-    // 创建管理员-lytest
-    admin_user.CreateAdminUserResponse1 CreateAdminUser1(1: admin_user.CreateAdminUserRequest1 req) (api.post="/eduosapi/admin_user/lytest/lytest1/lytest2/lytest3/lytest4/lytest5/lytest6/lytest7", api.name="创建管理员_lytest")
+    // 创建管理员
+    admin_user.CreateAdminUserResponse CreateAdminUser(1: admin_user.CreateAdminUserRequest req) (api.post="/eduosapi/admin_user/lytest", api.name="创建管理员lytest")
     // 相同请求方式及请求路径测试
-    admin_user.CreateAdminUserResponse2 CreateAdminUser2(1: admin_user.CreateAdminUserRequest2 req)  (api.post="/samepath", api.name="相同路径_lytest")
+    lytest.GetLogFileTreeResponse GetLogFileTree2(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/samepath", api.name="相同路径_lytest")
     // 相同请求方式及请求路径测试2
-    admin_user.CreateAdminUserResponse3 CreateAdminUser3(1: admin_user.CreateAdminUserRequest3 req)  (api.post="/samepath", api.name="相同路径_lytest")
-     // 业务测试
-    admin_user.CreateAdminUserResponse4 CreateAdminUser4(1: admin_user.CreateAdminUserRequest3 req)  (api.post="/status/200", api.name="返回200")
+    lytest.GetLogFileTreeResponse GetLogFileTree3(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/samepath", api.name="相同路径_lytest2")
+    // 业务测试
+    lytest.GetLogFileTreeResponse GetLogFileTree4(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/status/200", api.name="返回200")
 }
