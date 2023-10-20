3aa1cca97eb7aef467b9a860839db5f14151a809
luyang6137 <luyang6137@qq.com>
2023-10-20 10:50:13 +0800

    Update lytest-alpha5.thrift

--- thrift_idl/lytest-alpha5.thrift
+++ thrift_idl/lytest-alpha5.thrift
@@ -4,11 +4,7 @@ include "lytest/lytest.thrift"
 
 service ApiService {
     // 测试1
-    lytest.GetLogFileTreeResponse GetLogFileTree1(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/test/path1", api.name="相同路径_lytest")
-    // 测试2
-    lytest.GetLogFileTreeResponse GetLogFileTree2(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/test/path2", api.name="相同路径_lytest2")
-    // 测试3
-    lytest.GetLogFileTreeResponse GetLogFileTree3(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/11", api.name="相同路径_lytest33")
+    lytest.GetLogFileTreeResponse GetLogFileTree1(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.get="/test/path", api.name="测试测试_lytest")
     // 业务测试
-    lytest.GetLogFileTreeResponse GetLogFileTree4(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/status/200", api.name="返回200")
+    // lytest.GetLogFileTreeResponse GetLogFileTree4(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/status/200", api.name="返回200")
 }
