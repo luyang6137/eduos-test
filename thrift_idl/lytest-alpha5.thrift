ef298e10aa066a718203e459796003e18a8a9053
luyang6137 <luyang6137@qq.com>
2023-10-20 15:43:25 +0800

    Update lytest-alpha5.thrift

--- thrift_idl/lytest-alpha5.thrift
+++ thrift_idl/lytest-alpha5.thrift
@@ -2,5 +2,5 @@ include "lytest/lytest.thrift"
 
 service ApiService {
     // 测试1
-    lytest.GetLogFileTreeResponse GetLogFileTree1(1: lytest.GetLogFileTreeRequest req) throws (1: common.Error err) (api.get="/test/path", api.name="测试测试_lytest")
+    lytest.GetLogFileTreeResponse GetLogFileTree1(1: lytest.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/test/path", api.name="测试测试_lytest")
 }
