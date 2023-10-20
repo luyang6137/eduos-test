9897ec4f0bda71f54da2a98f58d72b4807f95c09
luyang6137 <luyang6137@qq.com>
2023-10-20 15:49:21 +0800

    Update lytest-alpha5.thrift

--- thrift_idl/lytest-alpha5.thrift
+++ thrift_idl/lytest-alpha5.thrift
@@ -1,9 +1,9 @@
-include "common.thrift"
+include "user/admin_user.thrift"
 include "lytest/lytest.thrift"
 
 service ApiService {
     // 测试1
     lytest.GetLogFileTreeResponse GetLogFileTree1(1: lytest.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/test/path", api.name="测试测试_lytest")
-    // 删除管理员-edit
-    common.EmptyResponse DeleteAdminUser(1: admin_user.DeleteAdminUserRequest req) (api.delete="/eduosapi/admin_user/:id", api.name="删除管理员_name")
+    // 创建管理员-edit
+    admin_user.CreateAdminUserResponse CreateAdminUser(1: admin_user.CreateAdminUserRequest req) (api.post="/eduosapi/admin_user", api.name="创建管理员_name")
 }
