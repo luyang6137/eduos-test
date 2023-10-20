1c36a35b33a61eb0364faa0eb39a7f6a0d223ecb
luyang6137 <luyang6137@qq.com>
2023-10-20 10:32:45 +0800

    Update lytest-alpha5.thrift

--- thrift_idl/lytest-alpha5.thrift
+++ thrift_idl/lytest-alpha5.thrift
@@ -7,6 +7,8 @@ service ApiService {
     lytest.GetLogFileTreeResponse GetLogFileTree2(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/test/path1", api.name="相同路径_lytest")
     // 测试2
     lytest.GetLogFileTreeResponse GetLogFileTree3(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/test/path2", api.name="相同路径_lytest2")
+    // 测试3
+    lytest.GetLogFileTreeResponse GetLogFileTree2(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/11", api.name="相同路径_lytest")
     // 业务测试
     lytest.GetLogFileTreeResponse GetLogFileTree4(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/status/200", api.name="返回200")
 }
