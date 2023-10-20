29219bd2832d4da31b0d2319215fe3e3424c03e4
luyang6137 <luyang6137@qq.com>
2023-10-20 10:53:10 +0800

    Update lytest-alpha5.thrift

--- thrift_idl/lytest-alpha5.thrift
+++ thrift_idl/lytest-alpha5.thrift
@@ -7,4 +7,6 @@ service ApiService {
     lytest.GetLogFileTreeResponse GetLogFileTree1(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.get="/test/path", api.name="测试测试_lytest")
     // 业务测试
     // lytest.GetLogFileTreeResponse GetLogFileTree4(1: logtree.GetLogFileTreeRequest req) throws (1: common.Error err) (api.post="/status/200", api.name="返回200")
+    // 删除管理员
+    common.EmptyResponse DeleteAdminUser(1: admin_user.DeleteAdminUserRequest req) (api.delete="/eduosapi/admin_user/:id", api.name="删除管理员_name")
 }
