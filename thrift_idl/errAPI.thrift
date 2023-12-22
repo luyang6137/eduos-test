b3aff7087fdaefa930089468598bad8f5aceb4b5
luyang6137 <luyang6137@qq.com>
2023-12-22 11:32:34 +0800

    Add files via upload

---
+++ thrift_idl/errAPI.thrift
@@ -0,0 +1,19 @@
+struct TestRequest {
+    // 名称
+	1: required string Name (api.header= 'Name')
+}
+
+struct TestResponse {
+    // 名称
+	1: required string Name (api.header= 'Name')
+}
+
+service ApiService {
+    TestResponse UITesta (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=getInfo", api.name="接口API111")
+    TestResponse UITestb.test (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=getInfo", api.name="接口API222")
+    TestResponse UITestc (1: TestRequest req) (api.get="/gitlab/alpha2/path1?", api.name="接口API333")
+    TestResponse UITestd (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action", api.name="接口API444")
+    TestResponse UITeste (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=test&Action=test", api.name="接口API555")
+    TestResponse UITestf (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=test&ID=test", api.name="接口API666")
+    TestResponse UITestg (1: TestRequest req) (api.get="/gitlab/alpha2/path1?ID=test&Action=test", api.name="接口API777")
+}
---
+++ thrift_idl/lytest-110-alpha2.thrift
@@ -0,0 +1,15 @@
+include "common.thrift"
+include "user/admin_user.thrift"
+include "order/user_order.thrift"
+include "lytest/lytest.thrift"
+
+service ApiService {
+    // 超长>8182
+    lytest.GetLogFileTreeResponse LongPathErr (1: lytest.GetLogFileTreeRequest req) (api.post="/eduosapi/110alpha2/path11", api.name="params超长")
+    // header数量=100
+    lytest.DemoResponse MuchHeaders (1: lytest.MuchHeaderRequest req) (api.post="/eduosapi/110alpha2/path22", api.name="header超多")
+    // header\params\body注解验证
+    lytest.DemoResponse AnnoVerify (1: lytest.AnnoRequest req) (api.post="/eduosapi/110alpha2/path33", api.name="注解验证")
+    // 路径正则验证
+    lytest.DemoResponse PathVerify (1: lytest.AnnoRequest req) (api.post="/eduosapi/110alpha2/path44?Action=ListInfo&Name=test&ID=123", api.name="路径正则验证")
+}
