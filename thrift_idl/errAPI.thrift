e2b9acf4888aa832125fbd8ec6ab70d9a2f5b998
luyang6137 <luyang6137@qq.com>
2023-12-27 11:47:42 +0800

    Update errAPI.thrift

--- thrift_idl/errAPI.thrift
+++ thrift_idl/errAPI.thrift
@@ -10,10 +10,10 @@ struct TestResponse {
 
 service ApiService {
     TestResponse UITesta (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=getInfo", api.name="接口API111")
-    TestResponse UITestb.test (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=getInfo", api.name="接口API222")
-    TestResponse UITestc (1: TestRequest req) (api.get="/gitlab/alpha2/path1?", api.name="接口API333")
-    TestResponse UITestd (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action", api.name="接口API444")
-    TestResponse UITeste (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=test&Action=test", api.name="接口API555")
-    TestResponse UITestf (1: TestRequest req) (api.get="/gitlab/alpha2/path1?Action=test&ID=test", api.name="接口API666")
-    TestResponse UITestg (1: TestRequest req) (api.get="/gitlab/alpha2/path1?ID=test&Action=test", api.name="接口API777")
+    TestResponse UITestb.test (1: TestRequest req) (api.get="/gitlab/alpha2/path2?Action=getInfo", api.name="接口API222")
+    TestResponse UITestc (1: TestRequest req) (api.get="/gitlab/alpha2/path3?", api.name="接口API333")
+    TestResponse UITestd (1: TestRequest req) (api.get="/gitlab/alpha2/path4?Action", api.name="接口API444")
+    TestResponse UITeste (1: TestRequest req) (api.get="/gitlab/alpha2/path5?Action=test&Action=test", api.name="接口API555")
+    TestResponse UITestf (1: TestRequest req) (api.get="/gitlab/alpha2/path6?Action=test&ID=test", api.name="接口API666")
+    TestResponse UITestg (1: TestRequest req) (api.get="/gitlab/alpha2/path7?ID=test&Action=test", api.name="接口API777")
 }
